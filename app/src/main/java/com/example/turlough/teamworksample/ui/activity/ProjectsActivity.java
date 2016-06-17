fd84e14e0e0ff31b90ce5a050bdd0e030d73635a
turlough <turloughcowman@gmail.com>
2016-06-17 09:58:01 +0100

    ProjectsActivity uses a ConstraintLayout instead

--- app/src/main/java/com/example/turlough/teamworksample/ui/activity/ProjectsActivity.java
+++ app/src/main/java/com/example/turlough/teamworksample/ui/activity/ProjectsActivity.java
@@ -35,7 +35,7 @@ public class ProjectsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teamwork);
+        setContentView(R.layout.activity_projects);
 
         init();
         loadProjects();
